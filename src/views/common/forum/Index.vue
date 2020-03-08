bd17d23b3b60bd238f557e40d134ed9cbf81854c
dingdazhuang <1021797587@qq.com>
2020-03-08 22:48:29 +0800

    game

---
+++ src/views/common/forum/Index.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="forum-index">
+        <div class="top-tab" id="forumTab">
+            <p :class="{ 'active': showIndex=='index' }" > <router-link to="/forum">交流讨论</router-link> </p>
+            <p :class="{ 'active': showIndex=='activity' }" ><router-link to="/forum/activity">社区活动</router-link></p>
+            <p :class="{ 'active': showIndex=='news' }" ><router-link to="/forum/news">网络爆料站</router-link> </p>
+            <p :class="{ 'active': showIndex=='game' }" ><router-link to="/forum/game">新游推荐</router-link></p>
+        </div>
+        <!-- <scroll class="view" :data="datalist"> -->
+            <transition name="slide-fade">
+                <keep-alive>
+                    <router-view></router-view>
+                </keep-alive>
+            </transition>
+        <!-- </scroll> -->
+    </div>
+</template>
+<script>
+export default {
+    // components: { Scroll },
+    data() {
+        return {
+            titleInfo: {
+                title: '掌动论坛',
+                showIcon: true,
+                hideReturnIcon: true,
+                icon: 'self-icon-comment-o fa-lg', //?????
+                link: '/editor',
+            },
+            showIndex: 'index',
+            datalist: [],
+        }
+    },
+    methods: {
+        ...mapActions([ 'handleTitle']),
+        _initTabStatus () {
+                let url = this.$route.fullPath,  page, showEditIcon=true;
+
+                switch(true) {
+                    case /forum\/activity/.test(url):    page = 'activity';
+                        break;
+                    case /forum\/news/.test(url):    page = 'news';  showEditIcon = false;
+                        break;
+                    case /forum\/game/.test(url):    page = 'game';  showEditIcon = false;
+                        break;
+                    case /forum/.test(url):   page = 'index';  
+                        break;
+                }
+                if (page) this.showIndex = page
+                this.handleTitle({
+                    title:    this.titleInfo.title, 
+                    hideReturnIcon: this.titleInfo.hideReturnIcon,
+                    showIcon: showEditIcon,
+                    icon: this.titleInfo.icon,
+                    link: this.titleInfo.link
+                })
+            },
+        
+    },
+    mounted() {
+        this._initTabStatus()
+
+        setTimeout(() => {
+            this.datalist = [12,344]
+        }, 5000);
+    },
+    watch: {
+        '$route': '_initTabStatus'
+    }
+}
+</script>
+<style lang="stylus">
+
+
+@import "../../common/css/index.scss";
+
+.forum-index {
+    position: relative;
+    font-size: $font-size-small; /*no*/
+    padding-top: 73px;
+    .top-tab {
+        position: fixed;
+        z-index: 1;
+        top: 100px;
+        width: 100%;
+        height: 70px;
+        line-height: 70px;
+        background-color: $bg-color-d;
+        padding: 0 50px;
+        border-bottom: 1px solid $text-color-orange;  /*no*/
+        @include box-sizing;
+        @include flex-between;
+        >p { 
+            font-size: $font-size-normal; /*no*/
+            font-weight: 500;
+            a, a:visited { color: black }
+            &.active {
+                a { color: $text-color-orange !important; }
+                font-weight: 800;
+            }
+        }
+    }
+    // .view { 
+        .slide-fade-enter-active {
+            transition: all .3s ease;
+        }
+        .slide-fade-leave-active {
+            transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+        }
+        .slide-fade-enter, .slide-fade-leave-to {
+            transform: translateX(10px);
+            opacity: 0;
+        } 
+    // }
+}
+</style>
---
+++ src/views/common/forum/index/News.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="news">
+        <div v-if="datalist">
+            <div class="top-news" v-if="datalist.spec_gamenews">
+                <p class="mark"><i>游民资讯</i></p>
+                <p class="dotted">{{`......`.repeat(30)}}</p>
+                <ul>
+                    <router-link tag="li" v-for="(spec, index) in datalist.spec_gamenews" :key="index" :to="'/forum/article/'+spec.acid">
+                        <div class="info">
+                            <h2 class="lines-nowrap">{{spec.activity}}</h2>
+                            <div>
+                                <p class="lines-nowrap">{{spec.intro}}</p>
+                                <p>{{spec.creattime | transformTime}}   <span> <i class="self-icon-eye"></i> {{spec.views | exchangeNumber }}</span> </p>
+                            </div>
+                        </div>
+                        <div class="img" v-if="spec.cover"><img :src="spec.cover" /></div>
+                    </router-link>
+                </ul>
+            </div>
+            <div class="game-news" v-if="datalist.normal_gamenews">
+                <p class="mark"><i>泛游戏</i></p>
+                <p class="dotted">{{`......`.repeat(30)}}</p>
+                <ul>
+                    <router-link tag="li" v-for="(item, index) in datalist.normal_gamenews" :key="index" :to="'/forum/article/'+item.acid">
+                        <div class="info">
+                            <h2 class="lines-nowrap">{{item.activity}}</h2>
+                            <div>
+                                <p class="lines-nowrap">{{item.intro}}</p>
+                                <p>{{item.creattime | transformTime}}   <span> <i class="self-icon-eye"></i> {{item.views | exchangeNumber }}</span> </p>
+                            </div>
+                        </div>
+                        <div class="img" v-if="item.cover"><img :src="item.cover" /></div>
+                    </router-link>     
+                </ul>
+            </div>
+        </div>
+        <div v-else style="margin-top: 1em; text-align:center">暂无资讯内容</div>
+    </div>    
+</template>
+
+<script>
+import * as api from 'api/api'
+import {formatDate} from 'common/js/tools'
+
+export default {
+    data() {
+        return {
+            datalist: null
+        }
+    },
+    methods: {
+        
+        
+    },
+    created() {
+        api.get("/forum/pubStand").then(res => {
+            if (res.code==200) {
+                this.datalist = res.data
+            }
+        })
+    },
+    filters: {
+        exchangeNumber: function(value) {
+            let number = value<100000 ? value : Math.floor(value/10000) + '万+'
+            return number
+        },
+        transformTime: function(time) {
+            return formatDate(time, "yyyy-MM-dd")
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../../common/css/index.scss";
+
+.news {
+    .dotted { color: $text-color-orange-d; padding-top: 30px;}
+    .mark {
+        position: absolute;
+        top: 25px;
+        left: 5%;
+        font-size: 0.95em;
+        i {
+            font-weight: 800;
+            display: inline-block;
+            width: 130px;
+            height: 45px;
+            line-height: 48px;
+            text-align: center;
+            color: #fff;
+            @include background-image(url('../../../common/images/fiction/bg_orange.png'));
+            background-size: 120px 42px;
+        }
+    }
+    .top-news { 
+        @include color-background;
+        position: relative;
+        .mark { @include color-background } 
+    }
+    .game-news { 
+         position: relative;
+        .mark { background: #ebebeb } 
+     }
+    ul {
+        padding: 0 40px;
+        li {
+            height: 205px;
+            @include box-sizing; 
+            border-bottom: 1px solid $text-color-orange;  /*no*/
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+            h2 { max-height: 2.2em;  }
+            .info {
+                height: 150px;
+                width: 50%;
+                @include flex-colum;
+                justify-content: space-between;
+                p { 
+                    text-align: justify;
+                    max-height: 2em;
+                    text-overflow: ellipsis;
+                    overflow: hidden;
+                    color: $text-color-dark;
+                }
+                div {
+                    p:last-child {
+                        font-size: $font-size-min;  /*no*/
+                        margin-top: 15px;
+                        span { float: right; }
+                    }
+                }
+            }
+            .img { 
+                width: 200px; height: 150px; overflow: hidden;
+                img { width: 100% } 
+            }
+            &:last-child { border: 0; }
+            &:first-child { border-bottom: 1px solid $text-color-orange;  /*no*/ }
+        }
+    }
+}
+</style>
+
